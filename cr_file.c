81d008e4f4272baeeb9a7ce9e937ccf9f1514470
wuting555 <wuting555@894e7f76-e486-11de-a347-f34a4f72eb7d>
2010-01-23 01:34:34 +0000

    add file operation

--- cr_file.c
+++ cr_file.c
@@ -1,46 +1,91 @@
 
 #include <linux/module.h>
 #include <linux/fs.h>
+
+
 extern struct dentry *auditfs_create_file(const char *name, mode_t mode,
 					struct dentry *parent, void *data,
 					const struct file_operations *fops);
+extern struct dentry *auditfs_create_dir(const char *name, struct dentry *parent);
 
 unsigned long __read_mostly     test_thresh;
 
-int test_open_generic(struct inode *inode, struct file *filp)
+static ssize_t default_read_file(struct file *file, char __user *buf,
+                                 size_t count, loff_t *ppos)
 {
+        return -1;
+}
 
-        filp->private_data = inode->i_private;
+static ssize_t default_write_file(struct file *file, const char __user *buf,
+                                   size_t count, loff_t *ppos)
+{
+        return count;
+}
+
+static int default_open(struct inode *inode, struct file *file)
+{
+        if (inode->i_private)
+                file->private_data = inode->i_private;
+
         return 0;
 }
 
-static const struct file_operations test_mark_fops = {
-        .open           = test_open_generic,
-//        .write          = test_mark_write,
+const struct file_operations auditfs_file_operations = {
+        .read =         default_read_file,
+        .write =        default_write_file,
+        .open =         default_open,
 };
+static int auditfs_u8_set(void *data, u64 val)
+{
+        *(u8 *)data = val;
+        return 0;
+}
+static int auditfs_u8_get(void *data, u64 *val)
+{
+        *val = *(u8 *)data;
+        return 0;
+}
+DEFINE_SIMPLE_ATTRIBUTE(fops_u8, auditfs_u8_get, auditfs_u8_set, "%llu\n");
+DEFINE_SIMPLE_ATTRIBUTE(fops_u8_ro, auditfs_u8_get, NULL, "%llu\n");
+DEFINE_SIMPLE_ATTRIBUTE(fops_u8_wo, NULL, auditfs_u8_set, "%llu\n");
+struct dentry *auditfs_create_u8(const char *name, mode_t mode,
+                                 struct dentry *parent, u8 *value)
+{
+        /* if there are no write bits set, make read only */
+        if (!(mode & S_IWUGO))
+                return auditfs_create_file(name, mode, parent, value, &fops_u8_ro);
+        /* if there are no read bits set, make write only */
+        if (!(mode & S_IRUGO))
+                return auditfs_create_file(name, mode, parent, value, &fops_u8_wo);
 
+        return auditfs_create_file(name, mode, parent, value, &fops_u8);
+}
+EXPORT_SYMBOL_GPL(auditfs_create_u8);
+static u8 auditfs_test;
+
+
+static struct dentry *d_audit;
 
 static __init int my_init_auditfs(void)
 {
-         struct dentry *d_tracer;
 
-        struct dentry *entry = NULL;
+	int error;
+	if(!d_audit)
+	 d_audit = auditfs_create_dir("audit", NULL);
 
+        if (!kernel_kobj) {
+                error = -ENOMEM;
+                return error;
+        }
 
-        entry = auditfs_create_file("auditfs_enabled", 0644, d_tracer,&test_thresh , &test_mark_fops);
-        if (!entry)
-                pr_warning("Could not create auditfs 'auditfs_enabled' entry\n");
-
-        entry = auditfs_create_file("auditfs_options", 0644, d_tracer,
-                                    &test_thresh, &test_mark_fops);
-        if (!entry)
-                pr_warning("Could not create auditfs 'auditfs_options' entry\n");
+	auditfs_create_u8("auditfs_test",0664,NULL,&auditfs_test);
 
 	return 0;
 }
 
 static void __exit my_exit_auditfs(void)
 {
+	
 	return;
 }
 
