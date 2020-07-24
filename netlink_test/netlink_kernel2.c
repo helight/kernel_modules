07da5485b803c2987a17bbd377c9c7fb614f8ce9
helight <helightxu@tencent.com>
2020-07-24 18:02:30 +0800

    fix

--- netlink_test/netlink_kernel2.c
+++ netlink_test/netlink_kernel2.c
@@ -32,44 +32,46 @@ struct netlink_data{
 
 static void test_link(struct sk_buff *skb)
 {
-        struct nlmsghdr *nlh;
-        u32             rlen;
-        void            *data;
+    struct nlmsghdr *nlh;
+    u32             rlen;
+    void            *data;
 
-         while (skb->len >= NLMSG_SPACE(0)) {
-                nlh = nlmsg_hdr(skb);
-                if (nlh->nlmsg_len < sizeof(*nlh) || skb->len < nlh->nlmsg_len)
-                        return;
-                rlen = NLMSG_ALIGN(nlh->nlmsg_len);
-                if (rlen > skb->len)
-                        rlen = skb->len;
-                data = NLMSG_DATA(nlh);
-                printk("link:%s", (char *)data);
-                netlink_ack(skb, nlh, 0);
-                skb_pull(skb, rlen);
-        }
+    while (skb->len >= NLMSG_SPACE(0)) {
+        nlh = nlmsg_hdr(skb);
+        if (nlh->nlmsg_len < sizeof(*nlh) || skb->len < nlh->nlmsg_len)
+                return;
+        rlen = NLMSG_ALIGN(nlh->nlmsg_len);
+        if (rlen > skb->len)
+                rlen = skb->len;
+        data = NLMSG_DATA(nlh);
+        printk("link:%s", (char *)data);
+        netlink_ack(skb, nlh, 0);
+        skb_pull(skb, rlen);
+    }
 }
 
 int __init init_link(void)
 {
-        xux_sock = netlink_kernel_create(&init_net, NETLINK_XUX, 0,
-                                           test_link, NULL, THIS_MODULE);
-        if (!xux_sock){
-                printk("cannot initialize netlink socket");
-                return -1;
-        } else
-                xux_sock->sk_sndtimeo = MAX_SCHEDULE_TIMEOUT;
+    struct netlink_kernel_cfg cfg = {
+		.input = test_link,//该函数原型可参考内核代码，其他参数默认即可
+	};
+    xux_sock = netlink_kernel_create(&init_net, NETLINK_XUX, cfg);
+    if (!xux_sock){
+        printk("cannot initialize netlink socket");
+        return -1;
+    } else
+        xux_sock->sk_sndtimeo = MAX_SCHEDULE_TIMEOUT;
 
 
-        printk("Init OK!\n");
-        return 0;
+    printk("Init OK!\n");
+    return 0;
 }
 
 void __exit exit_link(void)
 {
-        netlink_kernel_release(xux_sock);
-        printk(": Release OK!\n");
-        return;
+    netlink_kernel_release(xux_sock);
+    printk(": Release OK!\n");
+    return;
 }
 
 MODULE_AUTHOR("Helight.Xu");
